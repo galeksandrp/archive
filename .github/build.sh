4ced99a68665bd17020fba146e89632058e8b95c
Alexander Georgievskiy <galeksandrp@gmail.com>
2024-02-13 16:27:03 +0300

    lol

--- .github/build.sh
+++ .github/build.sh
@@ -2,9 +2,10 @@
 
 SCRIPT_FILEPATH="$0"
 VCS_DIRPATH="$PWD/vcs"
-VCS_HOME_DIRPATH="$HOME/vcs"
+VCS_HOME_DIRPATH="$HOME/vcs-dotdir"
 VCS_DATE='Jan 1 00:00:00 2000 +0000'
-VCS_REMOTE='https://github.com/galeksandrp/archive.git'
+VCS_REMOTE='https://github.com/$GITHUB_REPOSITORY_OWNER/archive.git'
+VCS_HTML_DIRPATH="$VCS_DIRPATH"
 
 # Getters functions
 
@@ -25,8 +26,6 @@ archivesToFolders() {
 # Other functions
 
 vcsInit() {
-    mkdir -p "$VCS_HOME_DIRPATH"
-
     mv "$VCS_DIRPATH/.git" "$VCS_HOME_DIRPATH"
     rm -rf "$VCS_DIRPATH"
     mkdir -p "$VCS_DIRPATH"
@@ -34,6 +33,7 @@ vcsInit() {
 
     (cd "$VCS_DIRPATH" \
         && git config --global core.autocrlf input \
+        && git config --global core.safecrlf false \
         && git init \
         && git remote add origin "$VCS_REMOTE")
 }
@@ -43,15 +43,15 @@ vcsInit() {
 archiveToFolder() {
     ARCHIVE_FILEPATH="$1"
 
-    mkdir -p "$VCS_DIRPATH/html"
-    mkdir -p "$VCS_DIRPATH/utf8"
+    mkdir -p "$VCS_HTML_DIRPATH"
+    # mkdir -p "$VCS_DIRPATH/utf8"
 
-    (echo "$ARCHIVE_FILEPATH" | grep '\.tar.gz$' > /dev/null) && (tar xf "$ARCHIVE_FILEPATH" -C "$VCS_DIRPATH/html" \
-        && chmod -R -x "$VCS_DIRPATH/html" \
+    (echo "$ARCHIVE_FILEPATH" | grep '\.tar.gz$' > /dev/null) && (tar xf "$ARCHIVE_FILEPATH" -C "$VCS_HTML_DIRPATH" \
+        && chmod -R -x "$VCS_HTML_DIRPATH" \
         # && tar xf "$ARCHIVE_FILEPATH" -C "$VCS_DIRPATH/utf8" \
         )
-    (echo "$ARCHIVE_FILEPATH" | grep '\.zip$' > /dev/null) && (unzip "$ARCHIVE_FILEPATH" -d "$VCS_DIRPATH/html" \
-        && chmod -R -x "$VCS_DIRPATH/html" \
+    (echo "$ARCHIVE_FILEPATH" | grep '\.zip$' > /dev/null) && (unzip -q "$ARCHIVE_FILEPATH" -d "$VCS_HTML_DIRPATH" \
+        && chmod -R -x "$VCS_HTML_DIRPATH" \
         # && unzip "$ARCHIVE_FILEPATH" -d "$VCS_DIRPATH/utf8" \
         )
 }
@@ -61,7 +61,18 @@ archiveToVCS() {
 
     vcsInit
     archiveToFolder "$ARCHIVE_FILEPATH"
-    # (cd "$VCS_DIRPATH/html" \
+
+    mv "$VCS_DIRPATH/.git" "$VCS_HOME_DIRPATH"
+    echo ">>> $ARCHIVE_FILEPATH" >> /tmp/lolol
+    find "$VCS_DIRPATH" -mindepth 1 -maxdepth 1 | wc -l >> /tmp/lolol
+    if [ "$(find "$VCS_DIRPATH" -mindepth 1 -maxdepth 1 | wc -l)" -eq 1 ]; then
+        rm -rf "$VCS_DIRPATH-old"
+        mv "$VCS_DIRPATH" "$VCS_DIRPATH-old"
+        mv "$VCS_DIRPATH-old/$(ls -a1 $VCS_DIRPATH)" "$VCS_DIRPATH"
+    fi
+    mv "$VCS_HOME_DIRPATH/.git" "$VCS_DIRPATH"
+
+    # (cd "$VCS_HTML_DIRPATH" \
     #     && find . -type f -exec sh -c "(uchardet '{}' | grep 'WINDOWS-1251' > /dev/null) && iconv -f windows-1251 -t utf-8 '{}' -o '$VCS_DIRPATH/utf8/{}'" \;)
     (cd "$VCS_DIRPATH" \
         && git -c core.autocrlf=input add . \
